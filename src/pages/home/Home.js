76bbc7bcd39b697f7235d8c67c332fcf7f861c9b
kedaroo <krb1202@gmail.com>
2022-03-27 20:26:14 +0530

    feat: recipe cards

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,21 @@
+import { useFetch } from '../../hooks/useFetch'
+
+// styles
 import './Home.css'
 
+// components
+import RecipeList from '../../components/RecipeList'
+
 export default function Home() {
+
+    const url = 'http://localhost:3000/recipes'
+    const { data, isPending, error} = useFetch(url)
+
     return(
-        <div>
-            Home
+        <div className='home'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>loading...</p>}
+            {data && <RecipeList recipes={data} />}
         </div>
     )
 }
